7bc95979f306011c19afbf6e1f2bb0035c609f69
Hyzeah Achungha <hyzeala28@gmail.com>
2026-06-22 22:11:23 +0100

    feat complete UI

---
+++ Mobile/src/app/_layout.jsx
@@ -0,0 +1,18 @@
+import { ClerkProvider } from "@clerk/clerk-expo"
+import { Slot } from "expo-router"
+import { tokenCache } from '@clerk/expo/token-cache'
+import { COLORS } from "../../constants/colors"
+import SafeScreen from "../../components/SafeScreen"
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider 
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
+    >
+      <SafeScreen style={{flex:1, backgroundColor: COLORS.background }}>
+        <Slot/>
+      </SafeScreen>
+    </ClerkProvider>
+  )
+}
